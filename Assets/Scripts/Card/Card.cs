c02261e17ef543128761edd78108f8f71bfcfe80
MacrazdHao <a695257347@gmail.com>
2024-10-03 23:24:58 +0800

    添加新属性

--- Assets/Scripts/Card/Card.cs
+++ Assets/Scripts/Card/Card.cs
@@ -22,7 +22,7 @@ public class Card : ICloneable
   public string id;
   public CardTypeMap.CardTypeEnum cardType; // 主类型
   public CardTypeMap.CardSubtypeEnum[] cardSubtype; // 副类型
-  public CardTypeMap.CardTypeEnum[] cardParentType; // 前置父类要求类型
+  public CardTypeMap.CardTypeEnum[] cardParentType; // 前置要求父类类型，暂时定义判断为满足其一即可
   public string cardName;
   public int cardPhysic;
   public int cardMagic;
